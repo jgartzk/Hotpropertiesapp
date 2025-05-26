037d2504d2ae27867833a02f0c6a41b912395d8c
ALANE10 <38444361+ALANE10@users.noreply.github.com>
2025-05-26 12:46:47 -0500

    sending work

--- final_project/src/main/java/edu/hotproperties/final_project/repository/UserRepository.java
+++ final_project/src/main/java/edu/hotproperties/final_project/repository/UserRepository.java
@@ -3,10 +3,12 @@ package edu.hotproperties.final_project.repository;
 
 import edu.hotproperties.final_project.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
